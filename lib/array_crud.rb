626381f87cdf4427ec171afc02b5b09b45d0547f
Melissa F <melissa.faulner@gmail.com>
2020-11-01 18:41:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   galaxy_battle = ["Rey", "Ben", "Finn", "Rose"]
-  galaxy_battle << "Poe"
+  galaxy_battle << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
